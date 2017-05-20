f57ade654147e5cb592c452d430b2ce024b5bb9e
lty96117 <sense.96117@gmail.com>
2017-05-20 21:46:33 +0800

    signin&up

--- src/frontend/static/js/init.js
+++ src/frontend/static/js/init.js
@@ -4,14 +4,32 @@
  */
 //set the menu btn for mobile
 $(".button-collapse").sideNav({
+  closeOnClick: true,
   draggable: true
 });
 
 $(document).ready(function () {
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
+  $('.modal').modal({
+    dismissible: true, // Modal can be dismissed by clicking outside of the modal
+    opacity: .5, // Opacity of modal background
+    inDuration: 300, // Transition in duration
+    outDuration: 200, // Transition out duration
+    startingTop: '0%', // Starting top style attribute
+    endingTop: '10%', // Ending top style attribute
+    ready: function (modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
+      window.dispatchEvent(new Event('resize'));
+      // alert("Ready");
+      // console.log(modal, trigger);
+
+    },
+    // complete: function () {
+    //   alert('Closed');
+    // } // Callback for Modal close
+  });
 });
 
-$(function() {
+$(function () {
   $('#mobile-nav').perfectScrollbar();
+  $('#modal1').perfectScrollbar();
 });
