b5504098250efba84e52b326bf85b4c6069a4e74
lty96117 <sense.96117@gmail.com>
2017-05-20 22:53:09 +0800

    init echarts

--- src/frontend/static/js/init.js
+++ src/frontend/static/js/init.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
   $('.modal').modal({
     dismissible: true, // Modal can be dismissed by clicking outside of the modal
-    opacity: .5, // Opacity of modal background
+    opacity: .3, // Opacity of modal background .5 default
     inDuration: 300, // Transition in duration
     outDuration: 200, // Transition out duration
     startingTop: '0%', // Starting top style attribute
