2390dbb68bc7b89e65f610624b0a4d48e2d7032f
lty96117 <sense.96117@gmail.com>
2018-01-02 15:16:54 +0800

    :sparkles: done

--- src/sql/config.js
+++ src/sql/config.js
@@ -1,7 +1,7 @@
 const config = {
   database: 'koa_demo',
-  username: 'lty96117',
-  password: 'password',
+  username: 'lty96117', // your username
+  password: 'password', // your password
   host: '127.0.0.1',
   port: 3306
 };
